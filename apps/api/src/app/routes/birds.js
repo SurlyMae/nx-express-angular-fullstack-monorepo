64ca3f2096d50d86527f892da6137f611c113411
Ashly LaMarr <surlymae@gmail.com>
2022-07-07 15:00:09 -0600

    WIP: switching Nest for Express

--- apps/api/src/app/routes/birds.js
+++ apps/api/src/app/routes/birds.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
   res.send('respond with a resource');
+});
+
+router.get('/about', (req, res) => {
+  res.send('respond with about resource');
 });
 
 module.exports = router;
--- apps/api/src/app/routes/index.js
+++ apps/api/src/app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.send('respond with home resource');
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
