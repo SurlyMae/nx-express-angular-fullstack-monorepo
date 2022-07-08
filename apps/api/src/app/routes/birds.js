231e254e29ac4b25c21def51801047f465cb738a
Ashly LaMarr <surlymae@gmail.com>
2022-07-08 11:02:17 -0600

    WIP: switching Nest for Express

--- apps/api/src/app/routes/birds.js
+++ apps/api/src/app/routes/birds.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res) {
-  res.send('respond with a resource');
+router.get('/data', (req, res) => {
+  res.send('create bird data');
 });
 
-router.get('/about', (req, res) => {
-  res.send('respond with about resource');
+router.get('/', getBirds);
+
+router.get('/:id', getBird);
+
+router.put('/', (req, res) => {
+  res.send('update bird');
 });
+
+router.post('/', function (req, res) {
+  res.send('add bird');
+});
+
+function getBirds(req, res) {
+  res.send('get birds');
+}
+
+function getBird(req, res) {
+  res.send(`get bird #${JSON.stringify(req.params)}`);
+}
 
 module.exports = router;
